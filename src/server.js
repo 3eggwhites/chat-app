bb273f41de413ffc78126d2078b3aaeeaf906ed7
Ayan Pal <contact@ayanpal.co.in>
2020-09-14 21:50:45 +0530

    added start command to package.json

--- src/server.js
+++ src/server.js
@@ -5,7 +5,8 @@ const port = process.env.PORT;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../public')));
+const publicDirectoryPath = path.join(__dirname, '../public');
+app.use(express.static(publicDirectoryPath));
 
 app.use('/', async (req,res) => {
     res.render('index');
